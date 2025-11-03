574bb570aa86e43ea128d01b09f5bdf04312dfca
hcxerry <montaboncherry238@gmail.com>
2025-11-03 18:21:12 +0800

    adding comments

--- Customer.java
+++ Customer.java
@@ -1,67 +1,82 @@
 public class Customer {
-    // Instance Variables
+
+    
     private String name;
     private int points;
 
-    // Static Variable
+    
     private static int totalCustomers = 0;
 
-    // ===== Constructors =====
-    // Default Constructor
+    
     public Customer() {
         this.name = "Unknown";
+        
         this.points = 0;
+        
         totalCustomers++;
     }
-
-    // Parameterized Constructor (name and points)
     public Customer(String name, int points) {
+        
         this.name = name;
+        
         this.points = points;
+        
         totalCustomers++;
     }
 
-    // Parameterized Constructor (name only)
+    
     public Customer(String name) {
         this.name = name;
+        
         this.points = 0;
+        
         totalCustomers++;
     }
 
-    // ===== Instance Methods =====
+    
     public void addPoints(int earnedPoints) {
+        
         this.points += earnedPoints;
     }
 
     public void redeemReward() {
+        
         if (points >= 100) {
+            
             System.out.println("Congratulations " + name + "! You redeemed a free drink!");
+            
             points -= 100;
+            
             System.out.println("Remaining Points: " + points);
         } else {
             System.out.println(name + " does not have enough points to redeem a drink.");
+            
             System.out.println("Current Points: " + points);
         }
     }
 
     public void displayCustomerInfo() {
         System.out.println("Customer Name: " + name);
+        
         System.out.println("Points: " + points);
+        
         System.out.println("---------------------------");
     }
 
     public int getPoints() {
+     
         return points;
     }
-
-    // ===== Static Methods =====
     public static void displayTotalCustomers() {
-        System.out.println("Total Registered Customers: " + totalCustomers);
+        
+        System.out.println("The Total Registered Customers: " + totalCustomers);
     }
 
     public static void getAllowedRewardsRedemption(Customer customer) {
+        
         int redeemable = customer.points / 100;
-        System.out.println(customer.name + " can redeem " + redeemable + " free drink(s).");
+        
+        System.out.println(customer.name + " can now redeem " + redeemable + " free drink(s).");
     }
 }
 
@@ -70,3 +85,4 @@ public class Customer {
 // AI THAT USE ChatGPT-5 
 // Conversation of AI 
 // https://chatgpt.com/share/68f4ad01-f3d0-8005-b1e7-7799dd423efe
+
